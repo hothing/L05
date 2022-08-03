186a657d43866a6c4d11fa7b72bb863a252224dd
Michael P <barbos@inbox.ru>
2022-08-03 09:48:52 +0300

    C0 language has been extended

--- C0Lang.fs
+++ C0Lang.fs
@@ -9,6 +9,12 @@ module C0Lang
     type C0Language = C0Program of C0Variable list * C0Statement list
     and C0Variable = string
     and C0Statement = C0Assign of C0Variable * C0Expression | C0Return of C0Argument
-    and C0Expression = C0Arg of C0Argument | C0Read | C0Minus of C0Argument | C0Add of C0Argument * C0Argument
+    and C0Expression = C0Arg of C0Argument 
+                        | C0Read 
+                        | C0Minus of C0Argument 
+                        | C0Add of C0Argument * C0Argument
+                        | C0Sub of C0Argument * C0Argument
+                        | C0Mul of C0Argument * C0Argument
+                        | C0Div of C0Argument * C0Argument
     and C0Argument = C0Int of int | C0Var of C0Variable
 
