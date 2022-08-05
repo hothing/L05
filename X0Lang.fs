361c3efec5174c1c0d74f648f10b2e705c78a38f
Michael P <barbos@inbox.ru>
2022-08-05 14:35:01 +0300

    X0 language translator, stage 0

--- X0Lang.fs
+++ X0Lang.fs
@@ -5,15 +5,17 @@ module X0Lang
     // instr ::= (addq arg arg) | (subq arg arg) | (movq arg arg) | (retq) | (negq arg) | (callq label) | (pushq arg) | (popq arg)
     // x86_0 ::= (program int instr+)
 
-    type X0Register = Rsp | Rbp | Rax | Rbx | Rcx | Rdx | Rsi | Rdi | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15
-    type X0Arg = X0Arg of int | X0ArgReg of X0Register | X0ArgDeref of X0Register * int
-    type X0Instr = AddQ of X0Arg * X0Arg 
-                 | SubQ of X0Arg * X0Arg 
-                 | MovQ of X0Arg * X0Arg 
-                 | RetQ 
-                 | NegQ of X0Arg
-                 | CallQ of string
-                 | PushQ of X0Arg
-                 | PopQ of X0Arg
-
+    type X0Language = X0Program of X0Variable list * X0Instruction list
+    and X0Variable = string
+    and X0Instruction = AddQ of X0Arg * X0Arg 
+                        | SubQ of X0Arg * X0Arg 
+                        | MovQ of X0Arg * X0Arg 
+                        | RetQ 
+                        | NegQ of X0Arg
+                        | CallQ of string
+                        | PushQ of X0Arg
+                        | PopQ of X0Arg
+    and X0Arg = X0Int of int | X0Reg of X0Register | X0Deref of X0Register * int | X0Var of X0Variable
+    and X0Register = Rsp | Rbp | Rax | Rbx | Rcx | Rdx | Rsi | Rdi | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15
+    
     
