ef654b21d1aa368ad3bcd3a20fc785bf8a8e73b2
Mykhayl Puzanov <barbos@inbox.ru>
2022-08-30 15:51:05 +0300

    Some algos for graph coloring

--- GraphColoring.fs
+++ GraphColoring.fs
@@ -2,15 +2,22 @@ module GraphColoring
 
     open ALGraph
 
+    let colorsAlgebra nonColor colorsCount =
+        let allColors = Set [ for i in (nonColor + 1) .. colorsCount -> i ]
+        let validColor color =
+            Set.contains color allColors
+        let includeColor color colorsSet =
+            if validColor color then
+                Set.add color colorsSet
+            else
+                colorsSet
+        let excludeColor color colorsSet = Set.remove color colorsSet
+        let complementColors = Set.difference allColors
+        (validColor, includeColor, excludeColor, complementColors)
+
     let makeColoredGraph nonColor aGraph  =
         let colorMap = Map.map (fun k v -> nonColor) aGraph
         (colorMap, aGraph)
-
-    let saturation nonColor vertice aColoredGraph =
-        let colorMap, aGraph = aColoredGraph
-        let adjVx = adjacents vertice aGraph
-        let colors = Set.map (fun v -> Map.find v colorMap) adjVx
-        Set.count (Set.filter (fun c -> c <> nonColor) colors)
 
     let setColor aColor aVrtice aColoredGraph =
         let colorMap, aGraph = aColoredGraph
@@ -24,5 +31,15 @@ module GraphColoring
     let color2 aVrtice aColoredGraph =
         let colorMap, aGraph = aColoredGraph
         Map.tryFind aVrtice colorMap
+
+    let adjacentColors validColor vertice aColoredGraph =
+        let colorMap, aGraph = aColoredGraph
+        let adjVx = adjacents vertice aGraph
+        let colors = Set.map (fun v -> Map.find v colorMap) adjVx
+        (Set.filter (fun c -> not (validColor c)) colors)
+    
+    let saturation validColor vertice aColoredGraph =
+        Set.count (adjacentColors validColor vertice aColoredGraph)
+    
 
     
